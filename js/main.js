5529725cfe665f6a94b0bcb4c6bf179602d2719f
Josue <21387996@uagro.mx>
2025-03-18 11:30:48 -0600

    correccion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 function loopDePares(numero){
     for (let i = 1; i<=100; i++){
+        console.log(i)
         if ((i + numero) %2 == 0){
-            console.log(`el ${numero+i} es par`)
+            console.log('el numero ' + (i+numero)+ ' es par')
         }
     }
 }
